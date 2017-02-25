e58be8f1308f1fa2a24cc6d0b091e01ba9ca1480
Anton Tsukanov <tsukanov.anton@gmail.com>
2017-02-25 21:37:01 +0300

    Removed redundant mutex unlock

--- code/engine.vc2008/utils/xrLC_Light/net_global_data_cleanup.h
+++ code/engine.vc2008/utils/xrLC_Light/net_global_data_cleanup.h
@@ -18,12 +18,9 @@ namespace lc_net
 		template<e_net_globals data>
 		void	set_cleanup					( u32 id )
 		{
-            std::lock_guard<decltype(lock)> locker(lock);
+                        std::lock_guard<decltype(lock)> locker(lock);
 			if( vec_cleanup[data] == id )
-			{
-				lock.Leave();
 				return;
-			}
 			++id_state;
 			vec_cleanup[data] = id;
 		};
