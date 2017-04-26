7e70534316b8529761f48f6e53478ad483f4aeb5
imdex <imdex@yandex-team.ru>
2017-04-26 04:07:49 +0300

    fixed xrGame build
    (cherry picked from commit 2e010d5a72128bf2b2205003521be8c846457e84)
    
    # Conflicts:
    #	README.md

--- code/engine/xrCore/xrMemory_subst_msvc.h
+++ code/engine/xrCore/xrMemory_subst_msvc.h
@@ -1,127 +1,23 @@
+#pragma once
+
 #ifdef DEBUG_MEMORY_NAME
-// new(0)
-template <class T>
-IC	T*		xr_new		()
+
+template <typename T, typename... Args>
+T* xr_new(Args&&... args)
 {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T();
+    T* ptr = static_cast<T*>(Memory.mem_alloc(sizeof(T), typeid(T).name()));
+    return new (ptr) T(std::forward<Args>(args)...);
 }
-// new(1)
-template <class T, class P1>
-IC	T*		xr_new		(const P1& p1) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1);
-}
-// new(2)
-template <class T, class P1, class P2> 
-IC	T*		xr_new		(const P1& p1, const P2& p2) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2);
-}
-// new(3)
-template <class T, class P1, class P2, class P3>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3);
-}
-// new(4)
-template <class T, class P1, class P2, class P3, class P4>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3,p4);
-}
-// new(5)
-template <class T, class P1, class P2, class P3, class P4, class P5>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3,p4,p5);
-}
-// new(6)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3,p4,p5,p6);
-}
-// new(7)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6, class P7>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6, const P7& p7) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3,p4,p5,p6,p7);
-}
-// new(8)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6, class P7, class P8>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6, const P7& p7, const P8& p8) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3,p4,p5,p6,p7,p8);
-}
-// new(9)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6, class P7, class P8, class P9>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6, const P7& p7, const P8& p8, const P8& p9) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T), typeid(T).name());
-	return new (ptr) T(p1,p2,p3,p4,p5,p6,p7,p8,p9);
-}
+
 #else // DEBUG_MEMORY_NAME
-// new(0)
-template <class T>
-IC	T*		xr_new		()
+
+template <typename T, typename... Args>
+T* xr_new(Args&&... args)
 {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T();
+    T* ptr = static_cast<T*>(Memory.mem_alloc(sizeof(T)));
+    return new (ptr) T(std::forward<Args>(args)...);
 }
-// new(1)
-template <class T, class P1>
-IC	T*		xr_new		(const P1& p1) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1);
-}
-// new(2)
-template <class T, class P1, class P2> 
-IC	T*		xr_new		(const P1& p1, const P2& p2) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2);
-}
-// new(3)
-template <class T, class P1, class P2, class P3>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3);
-}
-// new(4)
-template <class T, class P1, class P2, class P3, class P4>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3,p4);
-}
-// new(5)
-template <class T, class P1, class P2, class P3, class P4, class P5>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3,p4,p5);
-}
-// new(6)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3,p4,p5,p6);
-}
-// new(7)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6, class P7>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6, const P7& p7) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3,p4,p5,p6,p7);
-}
-// new(8)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6, class P7, class P8>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6, const P7& p7, const P8& p8) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3,p4,p5,p6,p7,p8);
-}
-// new(9)
-template <class T, class P1, class P2, class P3, class P4, class P5, class P6, class P7, class P8, class P9>
-IC	T*		xr_new		(const P1& p1, const P2& p2, const P3& p3, const P4& p4, const P5& p5, const P6& p6, const P7& p7, const P8& p8, const P8& p9) {
-	T* ptr	= (T*)Memory.mem_alloc(sizeof(T));
-	return new (ptr) T(p1,p2,p3,p4,p5,p6,p7,p8,p9);
-}
+
 #endif // DEBUG_MEMORY_NAME
 
 template <bool _is_pm, typename T>
