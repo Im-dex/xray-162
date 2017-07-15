98f5477abac38b807e22b4a699b6abdf200bd9db
imdex <imdex@yandex-team.ru>
2017-07-15 04:11:01 +0300

    Small refactoring

--- code/engine/xrPhysics/tri-colliderknoopc/dTriColliderCommon.h
+++ code/engine/xrPhysics/tri-colliderknoopc/dTriColliderCommon.h
@@ -9,12 +9,21 @@ extern xr_vector<flags8> gl_cl_tries_state;
 
 // extern xr_vector<int>::iterator		I,E,B				;
 
-#define CONTACT(Ptr, Stride) ((dContactGeom*)(((char*)Ptr) + (Stride)))
+// NOTE: stride is sizeof(dContact) * N, where N is [0, ...)
+
+inline dContactGeom* CONTACT(dContactGeom* ptr, const int stride) {
+    const size_t count = stride / sizeof(dContact);
+    dContact* contact = (dContact*)(uintptr_t(ptr) - uintptr_t(offsetof(dContact, geom)));
+    return &(contact[count]).geom;
+}
 //#define SURFACE(Ptr, Stride) ((dSurfaceParameters*) (((char*)Ptr) +
 //(Stride-sizeof(dSurfaceParameters))))
-#define SURFACE(Ptr, Stride)              \
-    ((dSurfaceParameters*)(((char*)Ptr) + \
-                           (Stride - offsetof(dContact, geom) + offsetof(dContact, surface))))
+inline dSurfaceParameters* SURFACE(dContactGeom* ptr, const int stride) {
+    const size_t count = stride / sizeof(dContact);
+    dContact* contact = (dContact*)(uintptr_t(ptr) - uintptr_t(offsetof(dContact, geom)));
+    return &(contact[count]).surface;
+}
+
 #define NUMC_MASK (0xffff)
 
 #define M_SIN_PI_3 REAL(0.8660254037844386467637231707529362)
