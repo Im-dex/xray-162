c49a6c2c66231f559fde18644112811b3e1d2a08
imdex <tsukanov.anton@gmail.com>
2017-12-10 23:06:14 +0300

    [xrCore] Numeric parsers refactoring [xrCore] Added parsers for all types of _vector*<T> and _color

--- code/engine/xrCore/conv.h
+++ code/engine/xrCore/conv.h
@@ -1,14 +1,123 @@
 #pragma once
 
 #include <boost/fusion/adapted.hpp>
+#include <boost/fusion/adapted/std_tuple.hpp>
 #include <boost/spirit/home/x3.hpp>
 
 #include <imdexlib/option.hpp>
 
-BOOST_FUSION_ADAPT_STRUCT(Fvector, x, y, z)
+BOOST_FUSION_ADAPT_TPL_STRUCT(
+    (T),
+    (_vector2) (T),
+    x, y
+)
+
+BOOST_FUSION_ADAPT_TPL_STRUCT(
+    (T),
+    (_vector3) (T),
+    x, y, z
+)
+
+BOOST_FUSION_ADAPT_TPL_STRUCT(
+    (T),
+    (_vector4) (T),
+    x, y, z, w
+)
+
+BOOST_FUSION_ADAPT_TPL_STRUCT(
+    (T),
+    (_color) (T),
+    r, g, b, a
+)
 
 enum class XrSkipWs;
 enum class XrNoSkip;
+
+namespace conv_register {
+
+#define REG_SIMPLE(Type, Parser)      \
+    template <>                       \
+    struct ParserFor<Type> {          \
+        using Skipper = XrNoSkip;     \
+        static const auto& parser() { \
+            return Parser;            \
+        }                             \
+    };
+
+template <typename T>
+struct ParserFor;
+
+REG_SIMPLE(std::int8_t, boost::spirit::x3::int8)
+REG_SIMPLE(std::int16_t, boost::spirit::x3::int16)
+REG_SIMPLE(std::int32_t, boost::spirit::x3::int32)
+REG_SIMPLE(std::int64_t, boost::spirit::x3::int64)
+REG_SIMPLE(std::uint8_t, boost::spirit::x3::uint8)
+REG_SIMPLE(std::uint16_t, boost::spirit::x3::uint16)
+REG_SIMPLE(std::uint32_t, boost::spirit::x3::uint32)
+REG_SIMPLE(std::uint64_t, boost::spirit::x3::uint64)
+REG_SIMPLE(float, boost::spirit::x3::float_)
+REG_SIMPLE(double, boost::spirit::x3::double_)
+
+template <typename T>
+struct ParserFor<_vector2<T>> {
+    using Skipper = XrSkipWs;
+    static const auto& parser() {
+        using namespace boost::spirit::x3;
+        static const auto res = rule<class _vector2Parser, _vector2<T>>()
+                              = ParserFor<T>::parser() >> ',' >> ParserFor<T>::parser();
+        return res;
+    }
+};
+
+template <typename T>
+struct ParserFor<_vector3<T>> {
+    using Skipper = XrSkipWs;
+    static const auto& parser() {
+        using namespace boost::spirit::x3;
+        static const auto res = rule<class _vector3Parser, _vector3<T>>()
+                              = ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser();
+        return res;
+    }
+};
+
+template <typename T>
+struct ParserFor<_vector4<T>> {
+    using Skipper = XrSkipWs;
+    static const auto& parser() {
+        using namespace boost::spirit::x3;
+        static const auto res = rule<class _vector4Parser, _vector4<T>>()
+                              = ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser();
+        return res;
+    }
+};
+
+template <typename T>
+struct ParserFor<_color<T>> {
+    using Skipper = XrSkipWs;
+    static const auto& parser() {
+        using namespace boost::spirit::x3;
+        static const auto res = rule<class _colorParser, _color<T>>()
+                              = ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser() >> ',' >>
+                                ParserFor<T>::parser();
+        return res;
+    }
+};
+
+#undef REG_SIMPLE
+
+} // conv_register namespace
+
+template <typename T>
+const auto& xrParserFor() {
+    return conv_register::ParserFor<T>::parser();
+}
 
 template <typename T, typename Parser, typename Skipper = XrNoSkip>
 imdex::option<T> xrParse(const std::string_view str, const Parser& parser, Skipper = Skipper()) {
@@ -25,80 +134,8 @@ imdex::option<T> xrParse(const std::string_view str, const Parser& parser, Skipp
     return res;
 }
 
-inline imdex::option<double> xrParseDouble(const std::string_view str) {
-    return xrParse<double>(str, boost::spirit::x3::double_);
-}
-
-inline imdex::option<float> xrParseFloat(const std::string_view str) {
-    return xrParse<float>(str, boost::spirit::x3::float_);
-}
-
-inline imdex::option<std::uint8_t> xrParseUint8(const std::string_view str) {
-    return xrParse<std::uint8_t>(str, boost::spirit::x3::uint8);
-}
-
-inline imdex::option<std::uint16_t> xrParseUint16(const std::string_view str) {
-    return xrParse<std::uint16_t>(str, boost::spirit::x3::uint16);
-}
-
-inline imdex::option<std::uint32_t> xrParseUint32(const std::string_view str) {
-    return xrParse<std::uint32_t>(str, boost::spirit::x3::uint32);
-}
-
-inline imdex::option<std::uint64_t> xrParseUint64(const std::string_view str) {
-    return xrParse<std::uint64_t>(str, boost::spirit::x3::uint64);
-}
-
-inline imdex::option<std::int8_t> xrParseInt8(const std::string_view str) {
-    return xrParse<std::int8_t>(str, boost::spirit::x3::int8);
-}
-
-inline imdex::option<std::int16_t> xrParseInt16(const std::string_view str) {
-    return xrParse<std::int16_t>(str, boost::spirit::x3::int16);
-}
-
-inline imdex::option<std::int32_t> xrParseInt32(const std::string_view str) {
-    return xrParse<std::int32_t>(str, boost::spirit::x3::int32);
-}
-
-inline imdex::option<std::int64_t> xrParseInt64(const std::string_view str) {
-    return xrParse<std::int64_t>(str, boost::spirit::x3::int64);
-}
-
-inline imdex::option<Fvector> xrParseFVector(const std::string_view str) {
-    using namespace boost::spirit::x3;
-    static const auto parser = float_ >> omit[char_(',')] >>
-                               float_ >> omit[char_(',')] >>
-                               float_;
-    return xrParse<Fvector>(str, parser, XrSkipWs());
-}
-
 template <typename T>
 imdex::option<T> xrParse(const std::string_view str) {
-    if constexpr (std::is_same_v<T, double>) {
-        return xrParseDouble(str);
-    } else if constexpr (std::is_same_v<T, float>) {
-        return xrParseFloat(str);
-    } else if constexpr (std::is_same_v<T, std::uint8_t>) {
-        return xrParseUint8(str);
-    } else if constexpr (std::is_same_v<T, std::uint16_t>) {
-        return xrParseUint16(str);
-    } else if constexpr (std::is_same_v<T, std::uint32_t>) {
-        return xrParseUint32(str);
-    } else if constexpr (std::is_same_v<T, std::uint64_t>) {
-        return xrParseUint64(str);
-    } else if constexpr (std::is_same_v<T, std::int8_t>) {
-        return xrParseInt8(str);
-    } else if constexpr (std::is_same_v<T, std::int16_t>) {
-        return xrParseInt16(str);
-    } else if constexpr (std::is_same_v<T, std::int32_t>) {
-        return xrParseInt32(str);
-    } else if constexpr (std::is_same_v<T, std::int64_t>) {
-        return xrParseInt64(str);
-    } else if constexpr (std::is_same_v<T, Fvector>) {
-        return xrParseFVector(str);
-    } else {
-        static_assert(false, "Type is unsupported");
-        return std::declval<T>();
-    }
+    using ParserFor = conv_register::ParserFor<T>;
+    return xrParse<T>(str, ParserFor::parser(), ParserFor::Skipper{});
 }
